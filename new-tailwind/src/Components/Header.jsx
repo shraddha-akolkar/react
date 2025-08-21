e3c2c466f5ac3743153f994e3a90da425d3f949b
Shraddha Akolkar <shraddhaaakolkar@gmail.com>
2025-08-21 23:19:00 +0530

    fix: update SVG attributes for compatibilityhanged

--- new-tailwind/src/Components/Header.jsx
+++ new-tailwind/src/Components/Header.jsx
@@ -59,9 +59,9 @@ const Header = () => {
 
           <button id="toggleOpen" className="lg:hidden cursor-pointer">
             <svg className="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
+              <path fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"></path>
+                clipRule="evenodd"></path>
             </svg>
           </button>
         </div>
